7e041db0feaf46a8feb3b66fb5584a8a61711ff2
mtmilenkoff <mtmilenkoff@outlook.com>
2022-07-04 11:10:49 -0300

    Add exercise 9

---
+++ Exercise9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise9
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 321321;
+
+            string s = num.ToString();
+
+            double total = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.WriteLine(s[i]);
+                total += char.GetNumericValue(s[i]);
+            }
+            Console.WriteLine(total);
+        }
+
+    }
+}
