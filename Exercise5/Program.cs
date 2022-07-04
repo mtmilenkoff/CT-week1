2f0c8f5a8a1b0803116653fccd8a8a008390c412
mtmilenkoff <mtmilenkoff@outlook.com>
2022-07-04 10:50:35 -0300

    Add exercise 5

---
+++ Exercise5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise5
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Today;
+
+            Console.WriteLine("Today {0} of {1} is {2} ", today.Day, today.Month, today.DayOfWeek);
+
+            DateTime nextDate = today.AddDays(40);
+
+            Console.WriteLine("in 40 days {0} of {1} is {2} ", nextDate.Day, nextDate.Month, nextDate.DayOfWeek);
+        }
+    }
+}
