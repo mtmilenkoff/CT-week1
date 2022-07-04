fd0037cbe0e1652fef9670eee865f3fc9fd376ee
mtmilenkoff <mtmilenkoff@outlook.com>
2022-07-04 10:39:59 -0300

    Add exercise 1

---
+++ Exercise1/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise1
+{
+    class Person
+    {
+        public string Name;
+        public short Age;
+
+        public void Greet() { Console.WriteLine("Hello, i'm {0}",this.Name); }
+        public void SetAge(short age) { this.Age = age; }
+        public void VerAge() { Console.WriteLine("My age is {0}", this.Age); }
+
+        public Person(string name, short age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ Exercise1/Teacher.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercise1
+{
+    internal class Teacher : Person
+    {
+        public Teacher(string name, short age) : base(name, age)
+        {
+        }
+
+        public void Explain()
+        {
+            Console.WriteLine("I'm explaining");
+        }
+    }
+}
