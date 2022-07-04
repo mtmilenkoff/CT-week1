a46151b089e3f9156d3e78e15da3df9d252239f4
mtmilenkoff <mtmilenkoff@outlook.com>
2022-07-04 11:08:00 -0300

    Add exercise 8

---
+++ Exercise8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise8
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReplaceWords("this_example", '_'));
+            Console.WriteLine(ReplaceWords("another.thing", '.'));
+        }
+
+        static string ReplaceWords(string word, char separate)
+        {
+            int index = word.IndexOf(separate);
+            string part1 = word.Substring(0, index);
+            string part2 = word.Substring(index + 1);
+
+            return (part2 + separate + part1);
+        }
+    }
+}
